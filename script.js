3f2e889613c6196d27e4183ec9b7dbc09c20203d
mm <matthias-mehdi.kaddour@epita.fr>
2023-02-13 21:11:56 +0100

    remove eval

--- script.js
+++ script.js
@@ -35,12 +35,66 @@ const observer = new IntersectionObserver((entries)=> {
  
  
  ///calc---------------------------------------
-
-document.getElementById('solve_b').onclick=function(){
-    if(document.getElementById('solve_i').value!=''){
-        var express = document.getElementById('solve_i').value;
-        document.getElementById('solve_r').value=eval(express);
-    }
-}
-
+ function demo_abac(exp){
+     var exp_l = exp.length;
+     var op1 = 0;
+     var op2 = 0;
+     var op ='';
+     var o_t=0;
+     for(let i = 0; i<exp_l; i++){
+         var curr = exp[i];
+         if(curr == '*' || curr== '/' || curr=='+' || curr== '-'){
+             o_t +=1;
+             op=curr;
+             if(o_t>1){
+                 break;
+             }
+         }
+         else if (curr!=' ' && (curr>='0' && curr<='9')){
+             if(o_t==0){
+                 op1*=10;
+                 op1+=curr-'0';
+             }
+             else{
+                 op2*=10;
+                 op2+=curr-'0';
+             }
+         }
+     }
+     if(op==''){
+         console.log(1);
+     }
+     else{
+         if(op=='/'){
+             return op1/op2;
+         }
+         else if(op=='*'){
+             return op1*op2;
+         }
+         else if(op=='+'){
+             return op1+op2;
+         }
+         else if(op=='-'){
+             return op1-1
+         }
+         else{
+             return -99;
+         }
+     }
+ 
+ }
+ document.getElementById('solve_b').onclick=function(){
+     if(document.getElementById('solve_i').value==''){
+         document.getElementById('solve_r').value=" "
+     }
+     else{
+         var express = document.getElementById('solve_i').value;
+         document.getElementById('solve_r').value=demo_abac(express);
+     }
+     
+ }
+ 
+ document.getElementsByClassName('').addEventListener('onmouseenter',function(){
+ 
+ })
 
